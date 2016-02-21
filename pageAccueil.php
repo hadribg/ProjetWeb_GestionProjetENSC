5a01e46ed02e67ddbe877626dd7c2ec1c7b1a3ef
Fny-lmrvn <fanlemorvan@ensc.fr>
2016-02-21 15:32:31 +0100

    Added files via upload

---
+++ pageAccueil.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ManaCog Project -  Gestion de projets de l'ENSC</title>
+    <link rel="stylesheet" href="accueil.css" />
+
+    <img src="logo.png" width="120" height="75" alt="logo">
+
+<ul id="menu-deroulant">
+    <li><a href="#">Accueil   </a>
+    </li>
+    <li><a href="#">l'ENSC</a>
+        <ul>
+            <li><a href="#">L'école</a></li>
+            <li><a href="#">Bordeaux INP</a></li>
+            <li><a href="#">Vivre à Bordeaux</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Mes projets</a>
+        <ul>
+            <li><a href="#">1ère année</a></li>
+            <li><a href="#">2ème année</a></li>
+            <li><a href="#">3ème année</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Mes enseignements</a>
+        <ul>
+            <li><a href="#">1ère année</a></li>
+            <li><a href="#">2ème année</a></li>
+            <li><a href="#">3ème année</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Mes professeurs</a>
+    </li>
+    <li><a href="#">SE CONNECTER</a>
+    </li>
+</ul>
+
+</head>
+<body>
+
+</body>
+</html>
+
